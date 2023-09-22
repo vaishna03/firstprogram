453f339a2c22725ec9445de7d33290362ccdfaf0
vaishna <113762032+vaishna03@users.noreply.github.com>
2023-09-22 20:25:12 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=100,b=10,i;
+    for(i=0;i<=a;i++)
+    {
+        a=a-b;
+        printf("%d\n",a);
+    }
+    printf("result:%d\n",i);
+
+return 0;
+}
